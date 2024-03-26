93fd4096a808e4134dd1bfba61ba37ed05a19b05
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-03-26 13:42:52 -0600

    cambios

---
+++ src/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'boletos_api',
+      script: 'dist/main.js',
+      instances: 'max',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
